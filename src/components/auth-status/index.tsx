30f0334d5c8a6085955ae52e709359b659ceb462
Paulo Ferraz <dev.paulo.ferraz@gmail.com>
2022-04-30 20:19:13 -0300

    bugfix: style dropdown user

--- src/components/auth-status/index.tsx
+++ src/components/auth-status/index.tsx
@@ -8,11 +8,13 @@ import useOuterClick from 'hooks/useOuterClick';
 export function AuthStatus() {
 	let { getCurrentAccount, signout } = useAuth();
 	const navigate = useNavigate();
-	const { theme, toggleUserDropDown } = useContext(ThemeContext) as ThemeContextType;
+	const { theme, toggleUserDropDown } = useContext(
+		ThemeContext
+	) as ThemeContextType;
 	const ref = useOuterClick<HTMLButtonElement>((e: any) => {
 		e.preventDefault();
 		if (theme.userDropDown) {
-			toggleUserDropDown()
+			toggleUserDropDown();
 		}
 	});
 
@@ -36,32 +38,50 @@ export function AuthStatus() {
 							alt='Header Avatar'
 						/>
 						<span className='d-none d-sm-inline-block ms-2'>John Doe</span>
-						<i className={`d-none d-sm-inline-block fa-solid fa-fw ${
-							theme.userDropDown ? 'fa-angle-up' : 'fa-angle-down'
-						}`}></i>
+						<i
+							className={`d-none d-sm-inline-block fa-solid fa-fw ${
+								theme.userDropDown ? 'fa-angle-up' : 'fa-angle-down'
+							}`}
+						></i>
 					</button>
 					<div
-						aria-labelledby="page-header-user-dropdown"
+						style={
+							theme.userDropDown
+								? {
+										position: 'absolute',
+										inset: '0px 0px auto auto',
+										margin: '0px',
+										transform: 'translate3d(0px, 45px, 0px)'
+								  }
+								: {}
+						}
+						aria-labelledby='page-header-user-dropdown'
 						className={`dropdown-menu dropdown-menu-md dropdown-menu-end p-0 border-0 ${
 							theme.userDropDown ? ' show' : ''
 						}`}
-						data-popper-placement="bottom-end">
-						<div className="p-3 text-center bg-body-light border-bottom rounded-top">
-							<p className="mt-2 mb-0 fw-medium">John Doe</p>
-							<p className="mb-0 text-muted fs-sm fw-medium">Developer</p>
+						data-popper-placement='bottom-end'
+					>
+						<div className='p-3 bg-body-light border-bottom rounded-top'>
+							<p className='mb-0 fw-medium'>John Doe</p>
+							<p className='mb-0 text-muted fs-sm fw-medium'>Developer</p>
 						</div>
-						<div className="p-2">
-							<Link to='/settings' className='dropdown-item d-flex align-items-center justify-content-between rounded'>
+						<div className='p-2'>
+							<Link
+								to='/settings'
+								className='dropdown-item d-flex align-items-center justify-content-between rounded'
+							>
 								Configurações
-								<i className='fa-solid fa-fw fa-cogs'></i>
 							</Link>
 						</div>
-						<div role="separator" className="dropdown-divider m-0"></div>
-						<div className="p-2">
-							<button onClick={() => {
-							signout();
-						}} className="dropdown-item d-flex align-items-center justify-content-between rounded">
-							<i className="fa-solid fa-fw fa-sign-out-alt"></i> Sair
+						<div role='separator' className='dropdown-divider m-0'></div>
+						<div className='p-2'>
+							<button
+								onClick={() => {
+									signout();
+								}}
+								className='dropdown-item d-flex align-items-center justify-content-between rounded'
+							>
+								Sair
 							</button>
 						</div>
 					</div>
